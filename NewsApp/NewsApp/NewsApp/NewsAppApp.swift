81bab2f5a28569ee6e619f447269482c50eba242
arju2ara <132534455+arju2ara@users.noreply.github.com>
2024-11-13 21:52:02 +0600

    Add files via upload

---
+++ NewsApp/NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Abu Loman Hossain Shuvo on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsApp/NewsApp/NewsApp/Views/ArticleRowView.swift
@@ -0,0 +1,7 @@
+//
+//  ArticleRowView.swift
+//  NewsApp
+//
+//  Created by Abu Loman Hossain Shuvo on 11/12/24.
+//
+
